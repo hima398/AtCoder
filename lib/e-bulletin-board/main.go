6992361926dc01346d8247cac89ae244dddcc116
hinata398 <r.hinata0398@gmail.com>
2020-10-22 22:08:46 +0900

    add test case

--- lib/e-bulletin-board/main.go
+++ lib/e-bulletin-board/main.go
@@ -2,100 +2,56 @@ package main
 
 import "fmt"
 
-//.### ..#. .### .### .#.# .### .### .### .### .###.
-//.#.# .##. ...# ...# .#.# .#.. .#.. ...# .#.# .#.#.
-//.#.# ..#. .### .### .### .### .### ...# .### .###.
-//.#.# ..#. .#.. ...# ...# ...# .#.# ...# .#.# ...#.
-//.### .### .### .### ...# .### .### ...# .### .###.
-
-//.###..#..###.###.#.#.###.###.###.###.###.
-//.#.#.##....#...#.#.#.#...#.....#.#.#.#.#.
-//.#.#..#..###.###.###.###.###...#.###.###.
-//.#.#..#..#.....#...#...#.#.#...#.#.#...#.
-//.###.###.###.###...#.###.###...#.###.###.
-
 var cMap = map[rune]int{
 	'.': 0,
 	'#': 1,
 }
-var m = map[int]int{
-	0x7555: 0,
-	0x2622: 1,
-	0x7174: 2,
-	0x7171: 3,
-	0x5571: 4,
-	0x7471: 5,
-	0x7475: 6,
-	0x7111: 7,
-	0x7575: 8,
-	0x7571: 9,
+var m = map[int]string{
+	0x7555: "0",
+	0x2622: "1",
+	0x7174: "2",
+	0x7171: "3",
+	0x5571: "4",
+	0x7471: "5",
+	0x7475: "6",
+	0x7111: "7",
+	0x7575: "8",
+	0x7571: "9",
 }
 
-func ParseNumber(s string) int {
+func ParseNumber(s string) string {
 	var v = 0
 	for _, c := range s {
 		v |= cMap[c]
 		v = v << 1
 	}
 	v = v >> 1
-	fmt.Printf("%x\n", v)
 	return m[v]
 }
 
-func ReadNumber(lines []string, n int) int {
-	var v = 0
-	for i := 0; i < n-1; i += 4 {
+func ReadNumber(lines []string, w int) string {
+	var v string
+	n := w - 1
+	for i := 0; i < n; i += 4 {
 		s := lines[0][i : i+4]
 		s += lines[1][i : i+4]
 		s += lines[2][i : i+4]
 		s += lines[3][i : i+4]
-		fmt.Println(s)
 		v += ParseNumber(s)
-		v *= 10
 	}
-	return v / 10
+	return v
 }
 
 func main() {
 	var n int
-	lines := make([]string, 5, 5)
 	fmt.Scan(&n)
+	width := 4*n + 1
+	lines := make([]string, width, width)
 	fmt.Scan(&lines[0])
 	fmt.Scan(&lines[1])
 	fmt.Scan(&lines[2])
 	fmt.Scan(&lines[3])
 	fmt.Scan(&lines[4])
 
-	fmt.Println(ReadNumber(lines, n))
+	fmt.Println(ReadNumber(lines, width))
 }
-
-/*
-func ParseNumber(s []string) int {
-	if s[0] == ".#.#" {
-		return 4
-	}
-	if s[1] == ".##." {
-		return 1
-	}
-	if s[3] == ".#.." {
-		return 2
-	}
-	// s[0] == ".###"
-	if s[1] == "...#" {
-		if s[2] == "...#" {
-			return 7
-		} else {
-			// s[3] == "...#"
-			return 3
-		}
-	} else if s[1] == ".#.." {
-		if s[3] == ".###" {
-			return 5
-		} else {
-			// .#.#
-			return 6
-		}
-	}
-	return 0
-}
-*/
--- lib/e-bulletin-board/main_test.go
+++ lib/e-bulletin-board/main_test.go
@@ -6,8 +6,23 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestReadNumber(t *testing.T) {
-	n := 5
+func TestReadNumber0(t *testing.T) {
+	n := 1
+	w := 4*n + 1
+	lines := []string{
+		".###.",
+		".#.#.",
+		".#.#.",
+		".#.#.",
+		".###.",
+	}
+
+	assert.Equal(t, "0", ReadNumber(lines, w))
+}
+
+func TestReadNumber1(t *testing.T) {
+	n := 1
+	w := 4*n + 1
 	lines := []string{
 		"..#..",
 		".##..",
@@ -16,5 +31,135 @@ func TestReadNumber(t *testing.T) {
 		".###.",
 	}
 
-	assert.Equal(t, 1, ReadNumber(lines, n))
+	assert.Equal(t, "1", ReadNumber(lines, w))
+}
+
+func TestReadNumber2(t *testing.T) {
+	n := 1
+	w := 4*n + 1
+	lines := []string{
+		".###.",
+		"...#.",
+		".###.",
+		".#...",
+		".###.",
+	}
+
+	assert.Equal(t, "2", ReadNumber(lines, w))
+}
+func TestReadNumber3(t *testing.T) {
+	n := 1
+	w := 4*n + 1
+	lines := []string{
+		".###.",
+		"...#.",
+		".###.",
+		"...#.",
+		".###.",
+	}
+
+	assert.Equal(t, "3", ReadNumber(lines, w))
+}
+func TestReadNumber4(t *testing.T) {
+	n := 1
+	w := 4*n + 1
+	lines := []string{
+		".#.#.",
+		".#.#.",
+		".###.",
+		"...#.",
+		"...#.",
+	}
+
+	assert.Equal(t, "4", ReadNumber(lines, w))
+}
+func TestReadNumber5(t *testing.T) {
+	n := 1
+	w := 4*n + 1
+	lines := []string{
+		".###.",
+		".#...",
+		".###.",
+		"...#.",
+		".###.",
+	}
+
+	assert.Equal(t, "5", ReadNumber(lines, w))
+}
+func TestReadNumber6(t *testing.T) {
+	n := 1
+	w := 4*n + 1
+	lines := []string{
+		".###.",
+		".#...",
+		".###.",
+		".#.#.",
+		".###.",
+	}
+
+	assert.Equal(t, "6", ReadNumber(lines, w))
+}
+func TestReadNumber7(t *testing.T) {
+	n := 1
+	w := 4*n + 1
+	lines := []string{
+		".###.",
+		"...#.",
+		"...#.",
+		"...#.",
+		"...#.",
+	}
+
+	assert.Equal(t, "7", ReadNumber(lines, w))
+}
+func TestReadNumber8(t *testing.T) {
+	n := 1
+	w := 4*n + 1
+	lines := []string{
+		".###.",
+		".#.#.",
+		".###.",
+		".#.#.",
+		".###.",
+	}
+
+	assert.Equal(t, "8", ReadNumber(lines, w))
+}
+func TestReadNumber9(t *testing.T) {
+	n := 1
+	w := 4*n + 1
+	lines := []string{
+		".###.",
+		".#.#.",
+		".###.",
+		"...#.",
+		".###.",
+	}
+
+	assert.Equal(t, "9", ReadNumber(lines, w))
+}
+
+func TestReadNumberInput1(t *testing.T) {
+	n := 10
+	w := 4*n + 1
+	lines := []string{
+		".###..#..###.###.#.#.###.###.###.###.###.",
+		".#.#.##....#...#.#.#.#...#.....#.#.#.#.#.",
+		".#.#..#..###.###.###.###.###...#.###.###.",
+		".#.#..#..#.....#...#...#.#.#...#.#.#...#.",
+		".###.###.###.###...#.###.###...#.###.###.",
+	}
+	assert.Equal(t, "0123456789", ReadNumber(lines, w))
+}
+
+func TestReadNumberInput2(t *testing.T) {
+	n := 29
+	w := 4*n + 1
+	lines := []string{
+		".###.###.###.###.###.###.###.###.###.#.#.###.#.#.#.#.#.#.###.###.###.###..#..###.###.###.###.###.#.#.###.###.###.###.",
+		"...#.#.#...#.#.#.#.#.#...#.#...#.#.#.#.#.#...#.#.#.#.#.#.#.....#.#.#.#.#.##..#.#...#.#.#...#.#...#.#...#.#.....#...#.",
+		".###.#.#...#.###.#.#.###.###...#.###.###.###.###.###.###.###...#.###.#.#..#..###...#.###.###.###.###.###.###.###.###.",
+		".#...#.#...#...#.#.#.#.#...#...#.#.#...#.#.#...#...#...#.#.#...#...#.#.#..#..#.#...#...#.#...#.#...#.#.....#...#.#...",
+		".###.###...#.###.###.###.###...#.###...#.###...#...#...#.###...#.###.###.###.###...#.###.###.###...#.###.###.###.###."}
+	assert.Equal(t, "20790697846444679018792642532", ReadNumber(lines, w))
 }
