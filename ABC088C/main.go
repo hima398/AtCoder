be103b8356df85842d173714d287964b7dbf032b
hinata398 <r.hinata0398@gmail.com>
2020-11-25 13:20:43 +0900

    coding ABC088C

---
+++ ABC088C/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextInt() int {
+	sc.Scan()
+	i, _ := strconv.Atoi(sc.Text())
+	return i
+}
+
+func main() {
+	buf := make([]byte, 1024*1024)
+	sc.Buffer(buf, bufio.MaxScanTokenSize)
+	sc.Split(bufio.ScanWords)
+	const n = 3
+	var c [n][n]int
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+
+		}
+	}
+
+}
