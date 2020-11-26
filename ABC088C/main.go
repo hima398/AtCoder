4e16858ae2c18884f0cb1d46fd71262d6b7c2bbb
hinata398 <r.hinata0398@gmail.com>
2020-11-26 22:50:44 +0900

    resolve ABC088C

--- ABC088C/main.go
+++ ABC088C/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -22,8 +23,28 @@ func main() {
 	var c [n][n]int
 	for i := 0; i < n; i++ {
 		for j := 0; j < n; j++ {
-
+			c[i][j] = nextInt()
 		}
 	}
-
+	var a [n]int
+	var b [n]int
+	a[0] = 0
+	for i := 0; i < n; i++ {
+		b[i] = c[0][i] - a[0]
+	}
+	for i := 1; i < n; i++ {
+		a[i] = c[i][0] - b[0]
+	}
+	result := true
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			result = result && (c[i][j] == a[i]+b[j])
+			if !result {
+				fmt.Println("No")
+				return
+			}
+		}
+	}
+	fmt.Println("Yes")
+	return
 }
