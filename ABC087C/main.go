7e359646edf5613b39fdec636127aab23a08cd95
hinata398 <r.hinata0398@gmail.com>
2020-12-12 13:42:01 +0900

    add dp to ABC 087C

--- ABC087C/main.go
+++ ABC087C/main.go
@@ -20,6 +20,42 @@ func nextString() string {
 	return sc.Text()
 }
 
+func Max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
+
+func FirstSubmission(n int, a1, a2 []int) int {
+	result := 0
+	for i := 0; i < n; i++ {
+		total := 0
+		for j := 0; j <= i; j++ {
+			total += a1[j]
+		}
+		for k := i; k < n; k++ {
+			total += a2[k]
+		}
+		if result < total {
+			result = total
+		}
+	}
+	return result
+}
+
+func DP(n int, a1, a2 []int) int {
+	var dp [2][100]int
+
+	dp[0][0] = a1[0]
+	dp[1][0] = dp[0][0] + a2[0]
+	for i := 1; i < n; i++ {
+		dp[0][i] = dp[0][i-1] + a1[i]
+		dp[1][i] = Max(dp[0][i]+a2[i], dp[1][i-1]+a2[i])
+	}
+	return dp[1][n-1]
+}
+
 func main() {
 	buf := make([]byte, 1024*1024)
 	sc.Buffer(buf, bufio.MaxScanTokenSize)
@@ -36,18 +72,6 @@ func main() {
 		a2[i] = nextInt()
 	}
 
-	result := 0
-	for i := 0; i < n; i++ {
-		total := 0
-		for j := 0; j <= i; j++ {
-			total += a1[j]
-		}
-		for k := i; k < n; k++ {
-			total += a2[k]
-		}
-		if result < total {
-			result = total
-		}
-	}
-	fmt.Println(result)
+	//	fmt.Println(FirstSubmission(n, a1, a2))
+	fmt.Println(DP(n, a1, a2))
 }
